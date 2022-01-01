00f540c3d911f095bc33bb3be775972c3003d110
Shahzaib Fardeen .G <58516484+Shahzaibfardeen@users.noreply.github.com>
2022-01-01 21:56:43 +0530

    Band Name Generator

--- Days/Day_1/Band_Name_Generator.py
+++ Days/Day_1/Band_Name_Generator.py
@@ -10,4 +10,3 @@ pet = input("What's your pet's name?\n")
 #4. Combine the name of their city and pet and show them their band name.
 #5. Make sure the input cursor shows on a new line.
 print("Your band name could be " + city + " " + pet)
- 
