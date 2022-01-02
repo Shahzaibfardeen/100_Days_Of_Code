edbf5a5c7bcf371b8e8c088f57290083cac0bf90
Shahzaib Fardeen .G <58516484+Shahzaibfardeen@users.noreply.github.com>
2022-01-02 21:09:56 +0530

    Tip Calculator

--- Days/Day_2/Tip Calculator.py
+++ Days/Day_2/Tip Calculator.py
@@ -17,4 +17,4 @@ total_bill = bill + total_tip_amount
 bill_per_person = total_bill / people
 final_amount = round(bill_per_person, 2)
 
-print(f"Each person should Pay: ${final_amount}")
+print(f"Each person should Pay: ${final_amount}") 
